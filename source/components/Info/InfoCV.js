fe794d1a86cedd473fe85cff94ebdee73a941b87
Bernard Bolter <bemajor@Bernards-iMac.local>
2017-01-18 15:46:42 -0800

    hard coded cv section in

--- source/components/Info/InfoCV.js
+++ source/components/Info/InfoCV.js
@@ -6,12 +6,160 @@ export default class InfoCV extends React.Component {
   }
   render() {
     return (
-      <section className="bio">
-        <div className="bio-long">
-          <p>THE CV ----- Contrary to popular fashion, I believe in an artistic practice that encompasses the long view. Sometimes, I feel like I’ve been fighting against this for most of my artistic career. Whether it was the art school that tried to break me down and rebuild me in their own vision, or the appeal of jumping on the latest trend to try and come up quicker, I remained on my own course.</p>
+      <section className="info-cv">
+        <div className="info-cv-name">
+          <h1>Bernard John Bolter IV</h1>
+          <p>(b. 1974, San francisco)</p>
         </div>
-        <div className="bio=short">
-          <p>Roll the Streets. Show teh Streets</p>
+        <div className="info-cv-list">
+          <h2 className="info-cv-header">Education</h2>
+
+          <div className="info-cv-row">
+            <h4>2008</h4>
+            <h3><span className="cv-bold">Gerrit Rietveld Academie</span>, Amsterdam, the Netherlands</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2000</h4>
+            <h3><span className="cv-bold">Hoge School voor de Kunsten</span>, Utrecht, the Netherlands</h3>
+          </div>
+
+          <h2 className="info-cv-header">Exhibitions</h2>
+
+          <div className="info-cv-row">
+            <h4>2017</h4>
+            <h3><span className="cv-bold">Book & Job Gallery</span>, Group Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2016</h4>
+            <h3><span className="cv-bold">Wonderland SF</span>, 2 Group Shows, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2016</h4>
+            <h3><span className="cv-bold">Open Studios</span>,  San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2016</h4>
+            <h3><span className="cv-bold">City Art Gallery</span>, 4 Group Shows, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2016</h4>
+            <h3><span className="cv-bold">Book & Job Gallery</span>, Group Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2015</h4>
+            <h3><span className="cv-bold">City Art Gallery</span>, 3 Group Shows, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2014</h4>
+            <h3><span className="cv-bold">Dickerman Prints</span>, Group Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2013</h4>
+            <h3><span className="cv-bold">Book & Job Gallery</span>, Solo Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2012</h4>
+            <h3><span className="cv-bold">Book & Job Gallery</span>, Solo Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2012</h4>
+            <h3><span className="cv-bold">Pecha Kucha</span>, Performance, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2011</h4>
+            <h3><span className="cv-bold">Neito Fine Art</span>, Group Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2012</h4>
+            <h3><span className="cv-bold">Pecha Kucha</span>, Performance, Shenzhen - China</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2011</h4>
+            <h3><span className="cv-bold">Vesuvios</span>, Solo Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2010</h4>
+            <h3><span className="cv-bold">Cutting Cultures</span>, Solo Show, Amsterdam</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2010</h4>
+            <h3><span className="cv-bold">Pecha Kucha</span>, Performance, Hamburg - Germany</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2009</h4>
+            <h3><span className="cv-bold">Cutting Cultures</span>, Group Show, Amsterdam</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2009</h4>
+            <h3><span className="cv-bold">Pecha Kucha</span>, Performance, Amsterdam</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2008</h4>
+            <h3><span className="cv-bold">Space Gallery</span>, Art Collision Group Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2007</h4>
+            <h3><span className="cv-bold">Cutting Cultures</span>, Group Show, Amsterdam</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2006</h4>
+            <h3><span className="cv-bold">Garage Gallery</span>, Art Collision Group Show, New York City</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2006</h4>
+            <h3><span className="cv-bold">Space Gallery</span>, Art Collision Group Show, San Francisco</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2005</h4>
+            <h3><span className="cv-bold">Lumi Art Space</span>, Solo Show, New York City</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2004</h4>
+            <h3><span className="cv-bold">Asterisk Art Space</span>, Art Collision Group Show, New York City</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2004</h4>
+            <h3><span className="cv-bold">Bolm Studios</span>, Art Collision Group Show, Austin</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2004</h4>
+            <h3><span className="cv-bold">Chela Gallery</span>, Art Collision Group Show, Baltimore</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2003</h4>
+            <h3><span className="cv-bold">Wohlfahrt Gallery</span>, Art Collision Group Show, Rotterdam - Netherlands</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2003</h4>
+            <h3><span className="cv-bold">d + n + r</span>, Art Collision Group Show, Amsterdam - Netherlands</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2003</h4>
+            <h3><span className="cv-bold">Gallery A123</span>, Art Collision Group Show, Leiden -  Netherlands</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2003</h4>
+            <h3><span className="cv-bold">Tribes Gallery</span>, Art Collision Group Show, New York City</h3>
+          </div>
+          <div className="info-cv-row">
+            <h4>2003</h4>
+            <h3><span className="cv-bold">Space 49</span>, Art Collision Group Show, New York City</h3>
+          </div>
+
+          <h2 className="info-cv-header">Publications</h2>
+
+          <div className="info-cv-row">
+            <h4>2013</h4>
+            <h3><span className="cv-bold">Digital City Series</span> - the book</h3>
+          </div>
+
+          <h2 className="info-cv-header">Organizations</h2>
+
+          <div className="info-cv-row">
+            <h4>2003 - 2008</h4>
+            <h3><span className="cv-bold">Art Collision</span>, co-founder of NYC based art group</h3>
+          </div>
+
         </div>
       </section>
     );
